b1a05cac562dcee1ddffd6afeb629674e5fa73ab
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-09-23 14:52:35 +0100

    rooks Red-03

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,42 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+
+
+            // find current location
+
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            for (int i = 1; i<=7 ; i++)
+            {
+                newPosition = Square.At(currentPosition.Row + i, currentPosition.Col);
+                availableSquares.Add(newPosition);
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                newPosition = Square.At(currentPosition.Row - i, currentPosition.Col);
+                availableSquares.Add(newPosition);
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                newPosition = Square.At(currentPosition.Row, currentPosition.Col + i);
+                availableSquares.Add(newPosition);
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                newPosition = Square.At(currentPosition.Row, currentPosition.Col - i);
+                availableSquares.Add(newPosition);
+            }
+
+            return availableSquares;
+            
+
+
         }
     }
 }
