8e770cbe1fbe991597e19256ec10048b5088bc8b
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-09-23 10:48:59 +0100

    made tests Red-01 Pass

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,41 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            //whats this 
+            //return Enumerable.Empty<Square>();
+            //var myEnumerable = new List<Square>();
+
+
+            Square newPosition;
+            
+
+            // find current location
+
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            if (Player == Player.White)
+            {
+               
+                //find square one up from it (which is 1 less in y co-ordinate
+                newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col);
+                //put that square in a list
+                availableSquares.Add(newPosition);
+
+            }
+            else
+            {
+                //find square one up from it (which is 1 less in y co-ordinate
+                newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col);
+                //put that square in a list
+                availableSquares.Add(newPosition);
+
+
+            }
+            return availableSquares;
+
+        
+
         }
     }
 }
